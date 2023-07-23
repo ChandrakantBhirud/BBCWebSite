550a866c24de097c68c1d4e1598b3dfe5ddea1b3
chanduPuneSoft@gmail.com <102297397+ChandrakantBhirud@users.noreply.github.com>
2023-07-23 22:14:57 +0100

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  on("file:preprocessor", browserify.default(config));
+  return config;
+}
+
+module.exports = defineConfig({  
+  e2e: {    
+      setupNodeEvents,
+      specPattern: 'cypress/integration/feature/*.feature',
+      nonGlobalStepDefinitions: true,
+      stepDefinitions:'cypress/support/step_definitions/'
+     },
+});
+
